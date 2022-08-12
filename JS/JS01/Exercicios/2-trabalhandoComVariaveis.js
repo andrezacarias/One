9c8da08840f18cdff7d492d048450d2bff434927
andrezacarias <104397428+andrezacarias@users.noreply.github.com>
2022-08-11 22:11:02 -0300

    Java Script

---
+++ JS/JS01/Exercicios/2-trabalhandoComVariaveis.js
@@ -0,0 +1,8 @@
+console.log("Trabalhando com variáveis");
+
+const idade=47;
+
+const nome = "André";
+
+//let ano = 2022;
+
---
+++ JS/JS01/Exercicios/5-atribuicaoDeVariaveis.js
@@ -0,0 +1,16 @@
+console.log("Trabalhando com atribuição de variáveis");
+
+const idade=47;
+let nome = "André";
+const sobrenome = "Zacarias";
+
+console.log(nome,sobrenome);
+
+console.log(`Meu nome é ${nome}`);
+
+nome = nome + sobrenome;
+
+console.log(nome);
+
+//let ano = 2022;
+
